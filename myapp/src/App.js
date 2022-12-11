3dd48896d93183482f133833ac695b8c9891646e
YoUttsu <114247237+YoUttsu@users.noreply.github.com>
2022-12-11 10:26:02 +0530

    react start

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='hover:bg-red-500'>hii does this work?</h1>
+    </>
+  );
+}
+
+export default App;
