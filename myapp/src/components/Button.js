b0222307421bae6f0a91af194e8383758edbbeda
YoUttsu <114247237+YoUttsu@users.noreply.github.com>
2022-12-22 09:45:23 +0530

    only chatty

--- myapp/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Mytext,{handleUpClick} from './Mytext'
-// import PropTypes from 'prop-types'
-
-export default function Button(props) {
-  return (
-   <button onClick={handleUpClick} className='mx-auto w-auto  h-auto p-3 bg-purple-300 hover:bg-purple-400 mt-5 shadow-md shadow-black'>{props.name}</button>
-  )
-}
